f8650465d99da01613c81cbb28b2a1937d3a64a1
Dasuni Dewani <dasunidewani1998@gmail.com>
2024-06-13 08:15:26 +0530

    bug fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableComponent } from "./table/table.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, TableComponent]
+    imports: [RouterOutlet, TableComponent, HttpClientModule]
 })
 export class AppComponent {
   title = 'data-table';
